26ee2c6f9fcb4c10e549359711ca04b4e0054e1c
Harold Sanchez Balaguera <haroldsanchezb@gmail.com>
2018-02-20 16:38:05 -0300

    New Medias
    www.eltiempo.com
    www.elespectador.com

--- content_script.js
+++ content_script.js
@@ -1,13 +1,34 @@
-var body = document.body;
-var canvas = document.querySelector('.off-canvas-wrap');
-var imgNano = document.querySelector('.img-nano');
-var modals = document.querySelectorAll('.md-modal');
-var overlay = document.querySelector('.md-overlay');
+let body = document.body;
+const page = window.location;
 
-body.classList.remove('blockContent');
-canvas.style.overflow = 'auto';
-imgNano.style.filter = 'none';
-overlay.style.display = 'none';
-modals.forEach(function(element) {
-    element.style.display = 'none';
-});
+if (page.hostname == 'www.semana.com') {
+    let canvas = document.querySelector('.off-canvas-wrap');
+    let imgNano = document.querySelector('.img-nano');
+    let modals = document.querySelectorAll('.md-modal');
+    let overlay = document.querySelector('.md-overlay');
+
+    body.classList.remove('blockContent');
+    canvas.style.overflow = 'auto';
+    imgNano.style.filter = 'none';
+    overlay.style.display = 'none';
+    modals.forEach(function(element) {
+        element.style.display = 'none';
+    });
+}
+
+if (page.hostname == 'www.elespectador.com') {
+    let modal = document.querySelector('.tp-modal');
+    let overlay = document.querySelector('.tp-backdrop');
+
+    overlay.style.display = 'none';
+    modal.style.display = 'none';
+}
+
+if (page.hostname == 'www.eltiempo.com') {
+    // TODO: rerun script to load article from server
+    let modal = document.querySelector('.paywallv2-container');
+
+    body.classList.remove('stop-scrolling');
+    modal.style.display = 'none';
+}
+
